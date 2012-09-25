853e67e91a4786cbaac301d02e925886f174351b
William DURAND <william.durand1@gmail.com>
2012-09-25 13:38:05 +0200

    Add tests, add travis config

--- spec/classes/stoplight_spec.rb
+++ spec/classes/stoplight_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe 'stoplight' do
   let(:title) { 'stoplight' }
+  let(:facts) { {
+    :concat_basedir => '/var/lib/puppet/concat',
+  } }
 
-  describe 'Test standard installation' do
-    let(:params) { {:ruby_version => '1.9.3' } }
-    it { should contain_package('make').with_ensure('present') }
+  describe 'Test another standard installation' do
+    let(:params) { {:ruby_version => '1.9.3-p194' } }
+
+    it { should contain_package('make').with_ensure('latest') }
+    it { should contain_package('g++').with_ensure('latest') }
+    it { should contain_user('stoplight') }
+    it { should contain_rvm_gem('bundler-1.9.3-p194') }
+    it { should contain_rvm_gem('rack-1.9.3-p194') }
+    it { should contain_exec('stoplight-install-git') }
+    it { should contain_exec('stoplight-install-bundler').with_command('rvm --with-rubies 1.9.3-p194 do bundle install') }
+    it { should contain_exec('stoplight-run').with_command('rvm --with-rubies 1.9.3-p194 do rackup ./config.ru &') }
+    it { should contain_concat('/home/stoplight/stoplight/config/servers.yml') }
   end
 end
