231fa6a073bc9470a4f748a622195ccb47a6097e
William DURAND <william.durand1@gmail.com>
2012-09-26 10:17:37 +0200

    Refactor main class to avoid inheritance

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,43 +16,57 @@
 #   }
 #
 class stoplight(
-  $ruby_version = $stoplight::params::ruby_version,
-  $user         = $stoplight::params::user
-) inherits stoplight::params {
+  $ruby_version = 'UNDEF',
+  $user         = 'UNDEF'
+) {
 
-  if ! defined(User[$user]) {
-    user { $user:
+  include stoplight::params
+
+  if $ruby_version == 'UNDEF' {
+    $sl_ruby_version = $stoplight::params::ruby_version
+  } else {
+    $sl_ruby_version = $ruby_version
+  }
+
+  if $user == 'UNDEF' {
+    $sl_user = $stoplight::params::user
+  } else {
+    $sl_user = $user
+  }
+
+  if ! defined(User[$sl_user]) {
+    user { $sl_user:
       ensure     => present,
       managehome => true,
     }
 
     rvm::system_user {
-      $user: ;
+      $sl_user: ;
     }
   }
 
   rvm_gem {
-    "bundler-${ruby_version}":
+    "bundler-${sl_ruby_version}":
       ensure        => present,
       name          => 'bundler',
-      ruby_version  => $ruby_version,
-      require       => Rvm_system_ruby[$ruby_version];
+      ruby_version  => $sl_ruby_version,
+      require       => Rvm_system_ruby[$sl_ruby_version];
   }
 
   rvm_gem {
-    "rack-${ruby_version}":
+    "rack-${sl_ruby_version}":
       ensure        => present,
       name          => 'rack',
-      ruby_version  => $ruby_version,
-      require       => Rvm_system_ruby[$ruby_version];
+      ruby_version  => $sl_ruby_version,
+      require       => Rvm_system_ruby[$sl_ruby_version];
   }
 
   exec { 'stoplight-install-git':
     command => 'git clone git://github.com/customink/stoplight.git',
     path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-    cwd     => "/home/${user}",
-    user    => $user,
-    unless  => "test -d /home/${user}/stoplight",
+    cwd     => "/home/${sl_user}",
+    user    => $sl_user,
+    unless  => "test -d /home/${sl_user}/stoplight",
     require => [
       Package['git'],
       User['stoplight']
@@ -60,34 +74,34 @@ class stoplight(
   }
 
   concat { 'stoplight-servers':
-    path    => "/home/${user}/stoplight/config/servers.yml",
-    owner   => $user,
-    group   => $user,
+    path    => "/home/${sl_user}/stoplight/config/servers.yml",
+    owner   => $sl_user,
+    group   => $sl_user,
     require => Exec['stoplight-install-git'],
   }
 
   exec { 'stoplight-install-bundler':
-    command => "rvm --with-rubies ${ruby_version} do bundle install",
+    command => "rvm --with-rubies ${sl_ruby_version} do bundle install",
     path    => '/usr/local/rvm/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-    cwd     => "/home/${user}/stoplight",
-    user    => $user,
-    unless  => "test -f /home/${user}/stoplight/config/servers.yml",
+    cwd     => "/home/${sl_user}/stoplight",
+    user    => $sl_user,
+    unless  => "test -f /home/${sl_user}/stoplight/config/servers.yml",
     require => [
       Exec['stoplight-install-git'],
-      Rvm_gem["bundler-${ruby_version}"]
+      Rvm_gem["bundler-${sl_ruby_version}"]
     ],
   }
 
   exec { 'stoplight-run':
-    command => "rvm --with-rubies ${ruby_version} do rackup ./config.ru &",
+    command => "rvm --with-rubies ${sl_ruby_version} do rackup ./config.ru &",
     path    => '/usr/local/rvm/bin:/usr/bin:/bin:/usr/sbin:/sbin',
-    cwd     => "/home/${user}/stoplight",
-    user    => $user,
+    cwd     => "/home/${sl_user}/stoplight",
+    user    => $sl_user,
     unless  => 'curl http://127.0.0.1:9292',
     require => [
       Exec['stoplight-install-bundler'],
       Concat['stoplight-servers'],
-      Rvm_gem["rack-${ruby_version}"]
+      Rvm_gem["rack-${sl_ruby_version}"]
     ],
   }
 }
